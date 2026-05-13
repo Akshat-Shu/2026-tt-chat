32c6d026dbac1c48e7cf0b5095a2c9e2e9183e16
Akshat-Shu <shuklaakshat.217@gmail.com>
2026-05-13 20:05:22 +0530

    Complete Question: send_and_receive_message

--- exercise-2/src/tcp_echo_client.cc
+++ exercise-2/src/tcp_echo_client.cc
@@ -43,6 +43,9 @@ void connect_to_server(int sock, sockaddr_in &server_address) {
 void send_and_receive_message(int sock, const std::string &message) {
   const int kBufferSize = 1024;
   // #Question - is buffer the best name we can use?
+  // #Answer   - No, We should specify what the buffer is for,
+  // for example, we can name it response_buffer to indicate that
+  // it stores the response from the server.
   char buffer[kBufferSize] = {0};
 
   // Send the message to the server
