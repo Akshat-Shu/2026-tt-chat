b1432f82e2f5d814cc5599c48e3e517dd907a2db
Akshat-Shu <shuklaakshat.217@gmail.com>
2026-05-13 12:24:54 +0530

    Update client to change message using command line arguments

--- exercise-1/src/tcp_echo_client.cc
+++ exercise-1/src/tcp_echo_client.cc
@@ -6,12 +6,16 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main() {
+int main(int argc, char *argv[])
+{
   // #Question - are these the same type?
-  // #Answer - No, message is a std::string which is a C++ 
-  // string class but "Hello from client" is a string literal 
+  // #Answer - No, message is a std::string which is a C++
+  // string class but "Hello from client" is a string literal
   // which is a constant character array.
   std::string message = "Hello from client";
+  if (argc > 1)
+    message = argv[1];
+
   const int kPort = 8080;
   const std::string kServerAddress = "127.0.0.1";
   sockaddr_in address;
@@ -19,19 +23,22 @@ int main() {
   char buffer[kBufferSize] = {0};
   // Creating socket file descriptor
   int my_sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (my_sock < 0) {
+  if (my_sock < 0)
+  {
     std::cerr << "Socket creation erron\n";
     return -1;
   }
   address.sin_family = AF_INET;
   address.sin_port = htons(kPort);
   // Convert IPv4 and IPv6 addresses from text to binary form
-  if (inet_pton(AF_INET, kServerAddress.c_str(), &address.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, kServerAddress.c_str(), &address.sin_addr) <= 0)
+  {
     std::cerr << "Invalid address/ Address not supported\n";
     return -1;
   }
   // Connect to the server
-  if (connect(my_sock, (sockaddr *)&address, sizeof(address)) < 0) {
+  if (connect(my_sock, (sockaddr *)&address, sizeof(address)) < 0)
+  {
     std::cerr << "Connection Failed\n";
     return -1;
   }
