7038be5d572f816ee1740083730cfc3df368c150
Akshat-Shu <shuklaakshat.217@gmail.com>
2026-05-13 20:07:19 +0530

    Update Question: read_args

--- exercise-2/src/tcp_echo_client.cc
+++ exercise-2/src/tcp_echo_client.cc
@@ -68,6 +68,8 @@ void send_and_receive_message(int sock, const std::string &message) {
 // instead so that any caller of the function can decide how to handle the error
 // Also, instead of just using the first argument for the message, we can 
 // concatenate all of the arguments to for the message.
+// We can also improve the function name to be more descriptive, 
+// for example, we can name it read_message_from_args.
 std::string read_args(int argc, char *argv[]) {
   std::string message = "Hello from client";
   if (argc == 1) {
