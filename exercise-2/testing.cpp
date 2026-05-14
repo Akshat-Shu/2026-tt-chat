5c7ed1520899861362617d3cf6e21e9fa37c2bfc
Akshat-Shu <shuklaakshat.217@gmail.com>
2026-05-14 15:51:43 +0530

    Complete Memory Management and Debug Mode in Your IDE

--- exercise-2/testing.cpp
+++ exercise-2/testing.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <string>
 
-int main() {
+struct Point
+{
+    int x, y;
+    std::string label;
+};
+
+int main()
+{
     const std::string message = "Hello, World!";
     std::cout << message << std::endl;
 
+    std::string point_label = "My Point";
+    Point point{10, 20, point_label};
     return 0;
 }
