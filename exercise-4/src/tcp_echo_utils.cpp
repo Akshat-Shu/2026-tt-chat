baad4d8b22be6cc37a8c2fad0fbc7f695d6b40f5
Akshat-Shu <shuklaakshat.217@gmail.com>
2026-06-02 23:02:38 +0530

    Seperate out common functionality

---
+++ exercise-4/src/tcp_echo_utils.cpp
@@ -0,0 +1,9 @@
+#include "tcp_echo_utils.h"
+
+
+template <typename T, typename S> void check_error(T test, S error_message) {
+  if (test) {
+    std::cerr << error_message << "\n";
+    exit(EXIT_FAILURE);
+  }
+}
