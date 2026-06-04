ac5c5a7d880a3334f43b921223b870164e53c2d4
Akshat-Shu <shuklaakshat.217@gmail.com>
2026-06-04 13:22:48 +0530

    Update Makefile to use libraries

--- exercise-4/src/tcp_echo_utils.cpp
+++ exercise-4/src/tcp_echo_utils.cpp
@@ -1,5 +1,5 @@
 #include "tcp_echo_utils.h"
-
+#include <cstdlib>
 
 template <typename T, typename S> void check_error(T test, S error_message) {
   if (test) {
@@ -7,6 +7,9 @@ template <typename T, typename S> void check_error(T test, S error_message) {
     exit(EXIT_FAILURE);
   }
 }
+
+template void check_error<bool, const char *>(bool, const char *);
+template void check_error<bool, std::string>(bool, std::string);
 
 int create_socket() {
   int sock = socket(AF_INET, SOCK_STREAM, 0);
